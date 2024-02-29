c0dfab1e81f1325e5c21c6869c6115a9b9648faf
Sankhadeep <sankhadeephazra02@gmail.com>
2024-02-29 12:01:24 +0530

    Added scroll animations and media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,41 @@ function scrollUp() {
 window.addEventListener('scroll', scrollUp)
 
 // SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')            
+        } else {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')            
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 
 // SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal('.home_header, .section_title', {delay: 600})
+sr.reveal('.home_footer', {delay: 750})
+sr.reveal('.home_img', {delay: 900, origin: 'top'})
+
+sr.reveal('.sponsor_img, .products_card, .footer_logo, .footer_content, .footer_copy', {origin: 'top', interval: 100})
+sr.reveal('.specs_data, .discount_animate', {origin: 'left', interval: 100})
+sr.reveal('.specs_img, .discount_img', {origin: 'right'})
+sr.reveal('.case_img', {origin: 'top'})
+sr.reveal('.case_data')
